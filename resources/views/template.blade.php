4025dad639487ac505c051e67debd7123645a1b6
Capitan Nemo <chepe263@users.noreply.github.com>
2018-08-09 20:53:27 -0600

    google analytics

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -399,6 +399,16 @@
 
     <!-- Custom scripts for this template -->
     <script src="/js/agency.min.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123735569-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-123735569-1');
+</script>
+
 
   </body>
 
